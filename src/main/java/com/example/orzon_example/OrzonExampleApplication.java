a0bf861f0a2529eaa4785ef812a861548d32bb47
Loris <160539775+zurich561@users.noreply.github.com>
2025-10-22 14:45:12 +0200

    spring boot framework

---
+++ src/main/java/com/example/orzon_example/OrzonExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.orzon_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrzonExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrzonExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/orzon_example/OrzonExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orzon_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrzonExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
